855ca4da01eb97a9bb8ff55026ffa92fb6c64cd4
Lucas <lucasamericano.dev@gmail.com>
2025-05-20 21:43:43 -0300

    refatoração do menu lateral
    Alterações pendentes de espaçamento

--- src/components/main/menu/menu.tsx
+++ src/components/main/menu/menu.tsx
@@ -11,41 +11,6 @@ export default function Menu() {
     const [colapsaItem, setColapsaItem] = useState(true)
     const [colapsaMenu, setColapsaMenu] = useState(true)
 
-    function MenuColapsado() {
-        return(
-            <>
-                <main className={corFundo? "menu bg-zinc-600 w-30 h-screen": "menu bg-zinc-600 w-30 h-screen"} >
-                    <div className="flex flex-row justify-center mb-12">
-                        <button onClick={()=>setColapsaMenu(!colapsaMenu)} className="botao"><MenuIcon size={30}/></button>
-                    </div>
-                    <div className="flex justify-center flex-1">
-                        {colapsaItem? <div className="duration-200 ease-in">
-                            <div className="flex flex-1 py-2 px-3 flex-col gap-3 justify-center">
-                                <div className="flex hover:bg-zinc-500 p-2 rounded-md text-lg justify-center cursor-pointer">
-                                    <a href="/contador"><BookCopy size={30}/></a>
-                                    <div className="hidden">
-                                        <MenuItem nomeLink='Contador' url="/contador"/>
-                                    </div>
-                                </div>
-                                <div className="flex hover:bg-zinc-500 p-2 rounded-md text-lg justify-center cursor-pointer">
-                                    <a href="/ranking"><ChartNoAxesCombined size={30}/></a>
-                                    <div className="hidden">
-                                        <MenuItem nomeLink='Ranking' url="/ranking"/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        : <div></div>}
-                    </div>
-                    <Link href={`/perfil`} className="flex justify-center">
-                        <img className="rounded-full" src="https://i.pinimg.com/736x/5c/9d/2a/5c9d2a13557b7330b3e91cf9a7670d1a.jpg" alt="Foto de perfil lucas" width={50} height={20}/>
-                    </Link>
-                </main>
-
-            </>
-        )
-    }
-
     useEffect (()=>{
         if (colapsaMenu === false && colapsaItem === false) {
             setColapsaItem(true)
@@ -54,32 +19,45 @@ export default function Menu() {
 
     return (
         <>
-        <section className="duration-400 ease-in-out">
-            {colapsaMenu? 
+        <section className="duration-400 ease-in-out"> 
             <main className={`${corFundo ? "menu":"menu"} ${colapsaMenu? "w-70": "w-30"}`} >
-                <div className="flex flex-row justify-between border-b-2 border-zinc-500 px-4 ">
-                    <h1 className="pb-5 text-2xl font-bold">Menu</h1>
-                    <button onClick={()=>setColapsaMenu(!colapsaMenu)} className="botao"><MenuIcon/></button>
+                <div className={`flex flex-row px-4 ${colapsaMenu ? "border-b-2 border-zinc-500 justify-between": "justify-center "}`}>
+                    {colapsaMenu && <h1 className="pb-5 text-2xl font-bold">Menu</h1>}
+                    <button onClick={()=>setColapsaMenu(!colapsaMenu)} className="botao"><MenuIcon size={30}/></button>
                 </div>
                 <div className="flex flex-col flex-1 mb-4">
-                    <div onClick={()=>setColapsaItem(!colapsaItem)} className="flex items-baseline justify-between p-2 cursor-pointer hover:text-zinc-300 duration-200">
-                        <h1 className="pt-3 text-lg font-medium">Funcionalidades</h1>
-                        <button className="cursor-pointer"><PlusIcon size={16}/></button>
+                    <div onClick={()=>setColapsaItem(!colapsaItem)} >
+                        {colapsaMenu && (
+                            <div className="flex items-baseline justify-between p-2 cursor-pointer hover:text-zinc-300 duration-200">
+                                <h1 className="pt-3 text-lg font-medium">Funcionalidades</h1>
+                                <button className="cursor-pointer"><PlusIcon size={16}/></button>
+                            </div>
+                        )}
                     </div>
                     
-                    {colapsaItem? <div className="duration-200 ease-in">
-                        <div className="flex py-2 px-3 flex-col gap-3">
-                            <div className="flex hover:bg-zinc-500 py-1 px-2 rounded-md text-lg items-center">
-                                <BookCopy size={20}/>
-                                <MenuItem nomeLink='Contador' url="/contador"/>
-                            </div>
-                            <div className="flex hover:bg-zinc-500 py-1 px-2 rounded-md text-lg items-center">
-                                <ChartNoAxesCombined size={20}/>
-                                <MenuItem nomeLink='Ranking' url="/ranking"/>
+                    {colapsaMenu ? (
+                        <div className="duration-200 ease-in">
+                            <div className="flex py-2 px-3 flex-col gap-3">
+                                <div className="flex hover:bg-zinc-500 py-1 px-2 rounded-md text-lg items-center">
+                                    <BookCopy size={20}/>
+                                    <MenuItem nomeLink='Contador' url="/contador"/>
+                                </div>
+                                <div className="flex hover:bg-zinc-500 py-1 px-2 rounded-md text-lg items-center">
+                                    <ChartNoAxesCombined size={20}/>
+                                    <MenuItem nomeLink='Ranking' url="/ranking"/>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    : <div></div>}
+                    ): (
+                            <div className="pt-6 gap-3">
+                                <div className="flex justify-center">
+                                    <BookCopy size={30}/>
+                                </div>
+                                <div className="flex justify-center">
+                                    <ChartNoAxesCombined size={30}/>
+                                </div>
+                            </div>
+                    )}
                 </div>
                 <Link href={`/perfil`} className="flex justify-around items-center text-center border-t-3 border-zinc-500 pt-3 cursor-pointer ">
                     <img 
@@ -96,7 +74,7 @@ export default function Menu() {
                         </div>
                     )}
                 </Link>
-            </main>: MenuColapsado()}
+            </main>
         </section>
         </>
     )
