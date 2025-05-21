f0eeefc7d2031dc20fdb1a2b80fade8e113e81c5
Lucas <lucasamericano.dev@gmail.com>
2025-05-21 12:04:35 -0300

    Refatoração

--- src/components/main/menu/menu.tsx
+++ src/components/main/menu/menu.tsx
@@ -19,14 +19,14 @@ export default function Menu() {
 
     return (
         <>
-        <section className="duration-400 ease-in-out"> 
+        <section className="duration-400 ease-in-out select-none"> 
             <main className={`${corFundo ? "menu":"menu"} ${colapsaMenu? "w-70": "w-30"}`} >
-                <div className={`flex flex-row px-4 ${colapsaMenu ? "border-b-2 border-zinc-500 justify-between": "justify-center "}`}>
+                <div className={`flex flex-row px-4 style ${colapsaMenu ? "border-b-2 border-zinc-500 justify-between": "justify-center "}`}>
                     {colapsaMenu && <h1 className="pb-5 text-2xl font-bold">Menu</h1>}
                     <button onClick={()=>setColapsaMenu(!colapsaMenu)} className="botao"><MenuIcon size={30}/></button>
                 </div>
                 <div className="flex flex-col flex-1 mb-4">
-                    <div onClick={()=>setColapsaItem(!colapsaItem)} >
+                    <div onClick={()=>setColapsaItem(!colapsaItem)}>
                         {colapsaMenu && (
                             <div className="flex items-baseline justify-between p-2 cursor-pointer hover:text-zinc-300 duration-200">
                                 <h1 className="pt-3 text-lg font-medium">Funcionalidades</h1>
@@ -34,9 +34,8 @@ export default function Menu() {
                             </div>
                         )}
                     </div>
-                    
                     {colapsaMenu ? (
-                        <div className="duration-200 ease-in">
+                        <div className={`transition-all duration-200 ease-in-out ${colapsaItem && 'hidden'}`}>
                             <div className="flex py-2 px-3 flex-col gap-3">
                                 <div className="flex hover:bg-zinc-500 py-1 px-2 rounded-md text-lg items-center">
                                     <BookCopy size={20}/>
@@ -49,19 +48,19 @@ export default function Menu() {
                             </div>
                         </div>
                     ): (
-                            <div className="pt-6 gap-3">
-                                <div className="flex justify-center">
+                            <div className="pt-10 gap-3 flex flex-col items-center">
+                                <a href="/contador" className="flex justify-center rounded-md p-3 w-fit hover:bg-zinc-500">
                                     <BookCopy size={30}/>
-                                </div>
-                                <div className="flex justify-center">
+                                </a>
+                                <a href="/ranking" className="flex justify-center rounded-md p-3 w-fit hover:bg-zinc-500">
                                     <ChartNoAxesCombined size={30}/>
-                                </div>
+                                </a>
                             </div>
                     )}
                 </div>
-                <Link href={`/perfil`} className="flex justify-around items-center text-center border-t-3 border-zinc-500 pt-3 cursor-pointer ">
+                <Link href={`/perfil`} className={`${colapsaMenu && "border-t-3 border-zinc-500"} flex justify-around items-center text-center pt-3 cursor-pointer`}>
                     <img 
-                        className="rounded-full" 
+                        className={`rounded-md`} 
                         src={LinkImagem = "https://i.pinimg.com/736x/5c/9d/2a/5c9d2a13557b7330b3e91cf9a7670d1a.jpg"} 
                         alt="Foto de perfil lucas" 
                         width={50} 
@@ -69,7 +68,7 @@ export default function Menu() {
                     />
                     {colapsaMenu && (
                         <div className="flex justify-between items-center">
-                            <h1 className="text-lg hover:text-zinc-300 duration-150">{LinkImagem = "Lucas Americano"}</h1>
+                            <h1 className="text-lg hover:text-zinc-300 duration-150">{NomeUsuario = "Lucas Americano"}</h1>
                             <ChevronsUpDown/>
                         </div>
                     )}
